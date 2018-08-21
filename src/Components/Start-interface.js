80e8d9421f5bcba313e6e92acc87793d84db391a
oscar orellana <saiphcita@gmail.com>
2018-08-20 18:18:50 -0600

    adding timeOut

--- src/Components/Start-interface.js
+++ src/Components/Start-interface.js
@@ -10,7 +10,9 @@ class StartInterface  extends Component {
         this.state = {
             color1: "#3C3B47",
             color2: "#3C3B47",
-            StatePage: <div className="divStatePage"><h2>Create a Worker ID to enter the work page and if you already have it, you can login.</h2></div>
+            StatePage: <div className="divStatePage"><h2>Create a Worker ID to enter the work page and if you already have it, you can login.</h2></div>,
+            listUsers: [],
+            pageTimeLoad: false
         };
       }
 
@@ -32,47 +34,60 @@ class StartInterface  extends Component {
             categorys.unshift("Select Category")
             this.setState({categorys : categorys})
         });
+        setTimeout(()=> {
+            this.setState({pageTimeLoad: true})
+        }, 700)
     }
 
-
+    
     ChangeToLogin(){
-        this.setState({color1: "#3BC079"});
-        this.setState({color2: "#3C3B47"});
-        this.setState({StatePage: <LogIn allUsers={this.state.allUsers} listUsers={this.state.listUsers}/>});
+        setTimeout(()=> {
+            this.setState({color1: "#3BC079"});
+            this.setState({color2: "#3C3B47"});
+            this.setState({StatePage: <LogIn allUsers={this.state.allUsers} listUsers={this.state.listUsers}/>});
+        }, 700)
     }
+
     ChangeToSingUp(){
-        this.setState({color2: "#3BC079"});
-        this.setState({color1: "#3C3B47"});
-        this.setState({StatePage:<SignUp 
-                                    allUsers={this.state.allUsers} 
-                                    listUsers={this.state.listUsers} 
-                                    posts={this.state.posts}
-                                    categorys ={this.state.categorys}
-                                    />});
+        setTimeout(()=> {
+            this.setState({color2: "#3BC079"});
+            this.setState({color1: "#3C3B47"});
+            this.setState({StatePage:<SignUp 
+                                        allUsers={this.state.allUsers} 
+                                        listUsers={this.state.listUsers} 
+                                        posts={this.state.posts}
+                                        categorys ={this.state.categorys}
+                                        />});
+        }, 700)
     }
 
     render(){
+        var pageLoad = <div style={{color: "white"}}>loading...</div>
+
+        if(this.state.pageTimeLoad){
+            pageLoad = <div>
+                            <div 
+                            style={{background: this.state.color1}} 
+                            className="DivButton" 
+                            onClick={()=>this.ChangeToLogin()}>
+                                Log In
+                            </div>
+                            <div
+                            style={{background: this.state.color2, margin: "0 0 0 6px"}} 
+                            className="DivButton" 
+                            onClick={()=>this.ChangeToSingUp()}>
+                                Sign Up
+                            </div>
+                        </div>
+        }else{
+            pageLoad = <div style={{color: "white"}}>loading...</div>
+        }
+
         return (
             <div className="DivBase ">
                 <h3>Welcome to WokerPage</h3>
                 <div className="DivForm">
-                    <div>
-
-                        <div 
-                        style={{background: this.state.color1}} 
-                        className="DivButton" 
-                        onClick={()=>this.ChangeToLogin()}>
-                            Log In
-                        </div>
-
-                        <div
-                        style={{background: this.state.color2, margin: "0 0 0 6px"}} 
-                        className="DivButton" 
-                        onClick={()=>this.ChangeToSingUp()}>
-                            Sign Up
-                        </div>
-
-                    </div>
+                    {pageLoad}
                     {this.state.StatePage}
                 </div>
             </div>
@@ -89,53 +104,69 @@ class LogIn extends Component{
             allUsers: this.props.allUsers,
             listUsers: this.props.listUsers,
             divErr: "",
-            url:"",
           };
         this.handleChangeUser = this.handleChangeUser.bind(this);
         this.handleChangePassword = this.handleChangePassword.bind(this);
       };
     
     handleClick = (event) => {
-        if(this.state.user.length === 0){
-            this.setState({divErr: <div style={{color: "red"}}>Write your Worker ID*</div> })
+        if(this.state.user.length === 0 && this.state.password.length === 0){
+            this.setState({divErr: <div style={{color: "red"}}>Write your Worker ID and your password*</div> })
         }else{
-            if(!this.state.listUsers.includes(this.state.user.toLowerCase())){
-                this.setState({divErr: <div style={{color: "red"}}>Your Worker ID doesn't exist*</div> })
+            if(this.state.user.length !== 0){
+                if(!this.state.listUsers.includes(this.state.user.toLowerCase())){
+                    this.setState({divErr: <div style={{color: "red"}}>Your Worker ID doesn't exist*</div> })
+                }else{
+                    if(this.state.password.length === 0){
+                        this.setState({divErr: <div style={{color: "red"}}>Enter your password*</div> })
+                    }else if(this.state.allUsers[this.state.numberForPassowrd].User.UserInfo.Password !== this.state.password){
+                        this.setState({divErr: <div style={{color: "red"}}>Your password doesn't match*</div> })
+                    }
+                };
             }else{
-                if(this.state.password.length === 0){
-                    this.setState({divErr: <div style={{color: "red"}}>Enter your correct password*</div> })
-                }
-            };
-        }
+                this.setState({divErr: <div style={{color: "red"}}>Enter your Worker ID*</div> })
+            }
+        };
     };
     
     handleChangeUser(e) {
         this.setState({ user: e.target.value.toLowerCase() });
         if(!this.state.listUsers.includes(e.target.value.toLowerCase())){
-            this.setState({divErr: <div style={{color: "red"}}>Your Worker ID doesn't exist*</div> })
-            this.setState({url: ""})
+            this.setState({divErr: <div style={{color: "red"}}>Your Worker ID is incorrect*</div> })
         }else{
-            this.setState({divErr: <div style={{color: "green"}}>Your Worker ID is correct*</div> })
+            this.setState({divErr: <div style={{color: "green"}}>Your Worker ID is correct</div> })
             this.setState({ user: e.target.value });
             this.setState({ numberForPassowrd: this.state.listUsers.indexOf(e.target.value.toLowerCase())});
         }
       };
       handleChangePassword(e) {
-        if(!this.state.listUsers.includes(this.state.user)){
-            this.setState({divErr: <div style={{color: "red"}}>Write your Worker ID correctly*</div> })
-            this.setState({url: ""})
-        }else{
-                if(this.state.allUsers[this.state.numberForPassowrd].User.UserInfo.Password !== e.target.value){
-                    this.setState({divErr: <div style={{color: "red"}}>Your password is incorrect*</div> })
-                }else{
-                    this.setState({divErr: <div style={{color: "green"}}>Your Password is correct*</div> })
-                    this.setState({ password: e.target.value });
-                    this.setState({url: "/postAndcategory/"+this.state.user})
-                }
-        }
+        this.setState({ password: e.target.value });
+        if(this.state.listUsers.includes(this.state.user)){
+            if(this.state.allUsers[this.state.numberForPassowrd].User.UserInfo.Password !== e.target.value){
+                this.setState({divErr: <div style={{color: "red"}}>Your password is incorrect*</div> })
+            }else{
+                this.setState({divErr: <div style={{color: "green"}}>Your Password is correct</div> })
+                this.setState({ password: e.target.value });
+            };
+        };
     };
 
     render(){
+        var divStatus = <div style={{display: "inline-block", float: "right"}}>{this.state.divErr}</div>;
+        var url = "";
+        var workerId = this.state.user;
+        var passwordId = this.state.password;
+        if(this.state.listUsers.includes(workerId)){
+            var number = this.state.listUsers.indexOf(workerId)
+            if (this.state.allUsers[number].User.UserInfo.Password === passwordId){
+                url = "/postAndcategory/"+workerId;
+                divStatus = <div style={{display: "inline-block", float: "right"}}><div style={{color: "green"}}>Welcome {workerId}</div></div>
+            }else{
+                url = "";
+            }
+        }else{
+            url = "";
+        };
         return (
             <div className="DivLogin">
                 <Form>
@@ -163,10 +194,10 @@ class LogIn extends Component{
                     </FormGroup>   
                          
                 </Form>
-                <Link to={this.state.url}>
+                <Link to={url}>
                 <Button color="success" onClick={this.handleClick}>Start</Button>
                 </Link>
-                <div style={{display: "inline-block", float: "right"}}>{this.state.divErr}</div>
+                {divStatus}
             </div> 
         );
     }
@@ -181,8 +212,7 @@ class SignUp extends Component{
             confirmPassword: "",
             replyErr: null,
             allUsers: this.props.allUsers,
-            listUsers: this.props.listUsers,
-            url:""
+            listUsers: this.props.listUsers
           };
 
           this.handleChangeUser = this.handleChangeUser.bind(this);
@@ -195,7 +225,7 @@ class SignUp extends Component{
             this.setState({replyErr: "You must fill all the fields*"})
         }else{
             if(this.state.listUsers.includes(this.state.user.toLowerCase())){
-                this.setState({replyErr: "This username already exists*"})
+                this.setState({replyErr: "This Worker ID already exists*"})
             }else{
                 if(this.state.password !== this.state.confirmPassword){
                     this.setState({replyErr: "Your password doesn't match*"})
@@ -226,11 +256,15 @@ class SignUp extends Component{
     };
     
     handleChangeUser(e) {
-        if(this.state.listUsers.includes(e.target.value.toLowerCase())){
-            this.setState({replyErr: "This Worker ID already exists*"})
+        this.setState({ user: e.target.value.toLowerCase()});
+        if(e.target.value.length !== 0){
+            if(this.state.listUsers.includes(e.target.value.toLowerCase())){
+                this.setState({replyErr: "This Worker ID already exists*"})
+            }else{
+                this.setState({replyErr: <div style={{color: "green"}}>Your Worker ID is correct.</div>})
+            }
         }else{
-            this.setState({replyErr: <div style={{color: "green"}}>Your Worker ID is correct.</div>})
-            this.setState({ user: e.target.value.toLowerCase()});
+            this.setState({replyErr: "Write your new Worker ID"})
         }
       };
 
@@ -240,16 +274,37 @@ class SignUp extends Component{
     };
 
     handleChangeConfirmPassword(e) {
+        this.setState({ confirmPassword: e.target.value });
         if(this.state.password !== e.target.value){
             this.setState({replyErr: "Your password doesn't match*"})
         }else{
             this.setState({replyErr: <div style={{color: "green"}}>Your Password is correct.</div>})
             this.setState({ confirmPassword: e.target.value });
-            this.setState({url: "/postAndcategory/"+this.state.user.toLowerCase()})
         }
     };
 
-    render(){    
+    render(){
+
+        var divStatus = <div className="replyErr">{this.state.replyErr}</div>;
+        var url = "";
+        var workerId = this.state.user;
+        var passwordId = this.state.password;
+        var confirmPasswordId = this.state.confirmPassword;
+        if(workerId.length !== 0){
+            if(!this.state.listUsers.includes(workerId)){
+                if(passwordId.length !== 0 || confirmPasswordId.length !== 0){
+                    if (confirmPasswordId === passwordId){
+                        url = "/postAndcategory/"+workerId;
+                        divStatus = <div className="replyErr"><div style={{color: "green"}}>Your Worker Id and your Password are correct</div></div>
+                    }else{
+                        url = "";
+                    }
+                }
+            }else{
+                url = "";
+            };
+        };
+
         return (
             <div className="DivSignup">
                 <Form >
@@ -287,10 +342,10 @@ class SignUp extends Component{
                     </FormGroup> 
 
                 </Form>
-                <Link to={this.state.url}>
+                <Link to={url}>
                 <Button style={{float: "right"}} color="success" onClick={this.handleClick}>Submit</Button>
                 </Link>
-                <div className="replyErr">{this.state.replyErr}</div>
+                {divStatus}
             </div> 
         );
     };
