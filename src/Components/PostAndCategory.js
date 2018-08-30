4bc38deb5df5634565f4bafa6877dffc2ac8be13
oscar orellana <saiphcita@gmail.com>
2018-08-29 20:42:08 -0600

    Improving state finished

--- src/Components/PostAndCategory.js
+++ src/Components/PostAndCategory.js
@@ -16,9 +16,7 @@ class PostAndCategory extends Component {
   }
 
   componentDidMount() {
-    const refUserPost = dbUser.ref("Users/"+this.props.numberUser+"/User/PostAndCategory/Post");
-    const refUserCategory = dbUser.ref("Users/"+this.props.numberUser+"/User/PostAndCategory/Category");
-    
+    const refUserPost = dbUser.ref("Users/"+this.props.numberUser+"/User/PostAndCategory/Post");    
     refUserPost.on("value", (snapshot) => {
       let posts = snapshot.val();
       this.setState({post : posts})
@@ -29,54 +27,69 @@ class PostAndCategory extends Component {
       // };
       // refUserPost.set(arrayxx);
     });
+
+    const refUserCategory = dbUser.ref("Users/"+this.props.numberUser+"/User/PostAndCategory/Category");
     refUserCategory.on("value", (snapshot) => {
       let category = snapshot.val();
       this.setState({category : category})
     });
+
     const refUserFinish = dbUser.ref("Users/"+this.props.numberUser+"/User");
     refUserFinish.on("value", (snapshot) => {
       let userFs = snapshot.val();
       this.setState({userFs: userFs})
+      //cartel constante felicitandolo
+      if(userFs.UserState === "finished"){ 
+        this.setState({fJob: true})
+        this.setState({heightPC: "82%"})
+      }else{ 
+        this.setState({fJob: false}) 
+      };
     });
-  }
+  };
 
   finishWork() {
     const refUserFinish = dbUser.ref("Users/"+this.props.numberUser+"/User");
     var arrayPost0 = []
     for (let i = 0; i < this.state.post.length; i++) { 
-      if(Number(this.state.post[i].category) === 0){
-        arrayPost0.push(i)
-      }
+      if(Number(this.state.post[i].category) === 0){ arrayPost0.push(i) };
     };
-    
     if(arrayPost0.length === 0){
       this.setState({finishJobM: 2})
       this.setState({heightPC: "84%"})
       let user = this.state.userFs
       user.UserState = "finished"
       refUserFinish.set(user);
+      this.setState({fJob: false}) 
     }else{
       this.setState({finishJobM: 1})
       this.setState({heightPC: "84%"})
       let user = this.state.userFs
       user.UserState = "working"
       refUserFinish.set(user);
+      this.setState({fJob: false}) 
     }
   };
 
-
   render() {
     var finishM = <div/>
+    var finishConstante = <div/>
+  
     if(this.state.finishJobM === 0){
       finishM= <div/>
     }else if(this.state.finishJobM === 1){
       finishM= <div style={{height:"4%", backgroundColor:"#E82704", color:"white"}}>Aun no has Terminado de Categorizar los Comentarios.</div>
     }else if(this.state.finishJobM === 2){
       finishM= <div style={{height:"4%", backgroundColor:"#4ECB0F", color:"white"}}>Felicidades, ya has termiando tu Trabajo.</div>
-    }
+    };
+
+    if(this.state.fJob){
+      finishConstante= <div style={{height:"6%", backgroundColor:"yellow", color:"black", paddingTop:"6px"}}>Muchas gracias por haber termiando tu trabajo.</div>
+    };
 
     return (
       <div style={{height:"92%", textAlign:"center"}}>
+        {finishConstante}
         {finishM}
         <div className="DivPostCategory" style={{height:this.state.heightPC, maxHeight:this.state.heightPC}}>
           <div className="titleList">
@@ -102,8 +115,13 @@ class PostAndCategory extends Component {
                       };
                       this.setState({post: newPost});
                       refUserPost.set(newPost);
-                      this.setState({finishJobM: 0})
-                      this.setState({heightPC: "88%"})
+                      this.setState({finishJobM: 0});
+                      this.setState({fJob: false});
+                      let user = this.state.userFs
+                      user.UserState = "working"
+                      user.PostAndCategory.Post = newPost
+                      const refUserFinish = dbUser.ref("Users/"+this.props.numberUser+"/User");
+                      refUserFinish.set(user);
                     }}
                   />}
                 </li>
